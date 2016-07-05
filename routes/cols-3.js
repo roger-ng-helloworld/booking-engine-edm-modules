78436d3481812495aae875e5c385473d495d7f17
Roger Ng <roger.ng@helloworld.com.au>
2016-07-05 15:32:56 +1000

    init

---
+++ routes/cols-3.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET bkgd-module page. */
+router.get('/', function(req, res) {
+  res.render('cols-3', { 
+  	//baseUrl: 'http://hwresponsiveedmmodules-hwrld.rhcloud.com/img/'
+  	//baseUrl: 'http://localhost:6323/img/'
+  	baseUrl: 'http://image.news.helloworld.com.au/lib/fe821272776d0d757c/m/2/'
+  });
+});
+
+module.exports = router;
